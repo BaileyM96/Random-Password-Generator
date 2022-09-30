e91cc7872d2fb01207b92ff33c48e12c7d42f643
Bailey Mejia <baileymejia28@gmail.com>
2022-09-30 13:55:29 -0700

    Added a for loop for the remaining chars and pushed it to the password array

--- script.js
+++ script.js
@@ -2,7 +2,11 @@
 var generateBtn = document.querySelector("#generate");
 
 
-function generatePassword() {  //This function is linked to the HTML code so the rest of the code goes into this function//
+function generatePassword() {  //This function is linked to the HTML code and the actual button to generate password so the rest of the code goes into this function//
+  var includeLowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var includeUpperCase= ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var includeNumbers = ['0', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var includeSpecialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
 
 // Had to include 5 questions for the prompt//
 var passWordLength = prompt('How long do you want your password to be?');
@@ -23,7 +27,7 @@ var options = {   // made an object container with the variables  to add to gene
 var possiblePassword = [];  // Made 2 new variables  to make empty arrays so we can add the 5 variables to them//
 var password = [];
 
-function selectRandomChar(array) {  //Made an array to be sure to always include at  least 1 of the chars from the variable//
+function selectRandomChar(array) {  //Made a new function and an array to be sure to always include at  least 1 of the chars from the variable//
    var randomNumber = Math.floor(Math.random() * array.length); //Used math so it can randomly pick a char from the variable//
    return array[randomNumber]; //gives us a random char from the array//
 }
@@ -47,17 +51,15 @@ if(options.includeLowerCase) { //And it just repeats 3 more times//
   }
 
 
+  var remainingChars = options.length - password.length;
 
+  for(var i = 0; i < remainingChars; i++) {
+    var randChar = selectRandomChar(possiblePassword);
+    password.push(randChar);
 
-// 4 password options //
-// Need a function to start the application //
-// var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-// var lowerCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-// var numbers = ['0', '2', '3', '4', '5', '6', '7', '8', '9'];
-// var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
 }
 
-
+}
 
 
 
@@ -66,13 +68,11 @@ function writePassword() {
 var password = generatePassword();
 var passwordText = document.querySelector("#password");
 
-passwordText.value = password;
-
+  passwordText.value = password;
 }
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-// [Math.floor(Math.random() * generatePassword.length (generatePassword++))];
